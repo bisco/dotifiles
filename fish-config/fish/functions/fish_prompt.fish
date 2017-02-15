f0b7ed7816c657ad180fe975e9c8812f23ce6d0b
bisco <nobu+github@glleu.net>
2017-02-15 22:46:45 +0900

    cleaning fish_prompt

--- fish-config/fish/functions/fish_prompt.fish
+++ fish-config/fish/functions/fish_prompt.fish
@@ -1,12 +1,11 @@
 function fish_prompt
     set_color cyan
-    printf "%s" (whoami)
-    printf "@%s " (hostname)
+    printf "%s@%s " (whoami) (hostname)
     set_color yellow
     printf "%s" (pwd)
     set_color normal
-    printf "%s " (__fish_git_prompt)
     if test -d .git
+        printf "%s " (__fish_git_prompt)
         set_color magenta
         printf "[%s]" (git rev-parse --short HEAD)
         set_color normal
