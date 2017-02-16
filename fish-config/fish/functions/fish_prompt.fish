003894dad00048d43b1eda9b744c872ab66ff8bc
bisco <nobu+github@glleu.net>
2017-02-16 22:37:48 +0900

    stop printing git hash

--- fish-config/fish/functions/fish_prompt.fish
+++ fish-config/fish/functions/fish_prompt.fish
@@ -4,11 +4,6 @@ function fish_prompt
     set_color yellow
     printf "%s" (pwd)
     set_color normal
-    if test -d .git
-        printf "%s " (__fish_git_prompt)
-        set_color magenta
-        printf "[%s]" (git rev-parse --short HEAD)
-        set_color normal
-    end
+    printf "%s " (__fish_git_prompt)
     printf "\n> "
 end
