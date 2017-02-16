4fc36812cc9c9cecb14f2442b249b82e5795f067
bisco <nobu+github@glleu.net>
2017-02-16 23:43:49 +0900

    support dynamic window rename on tmux with fish

--- fish-config/fish/config.fish
+++ fish-config/fish/config.fish
@@ -11,3 +11,10 @@ set __fish_git_prompt_showupstream 'yes'
 set __fish_git_prompt_showuntrackedfiles 'yes'
 set __fish_git_prompt_show_informative_status 'yes'
 set __fish_git_prompt_showcolorhints 'yes'
+
+# rename tmux window
+function window_rename --on-event fish_preexec
+    if test -n (echo $TERM | grep -e screen -e tmux)
+        tmux rename-window $argv[1]
+    end
+end
