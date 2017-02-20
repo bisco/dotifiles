ada9d406b71a3a6ebe11196475fcedacc212e690
bisco <nobu+github@glleu.net>
2017-02-20 23:27:55 +0900

    use full path when issuing ln command

--- install.sh
+++ install.sh
@@ -7,8 +7,17 @@ for i in ${DOTFILES[@]};
 do
     if [ -d $i ]; then
         # fish-config 
-        ln -s $CURDIR/$i ~/.config
-        echo ln -s $CURDIR/$i ~/.config
+        mkdir -p ~/.config/fish
+        echo mkdir -p ~/.config/fish
+        ln -s $CURDIR/$i/fish/config.fish ~/.config/fish/config.fish
+        echo ln -s $CURDIR/$i/fish/config.fish ~/.config/fish/config.fish
+        mkdir -p ~/.config/fish/functions
+        echo mkdir -p ~/.config/fish/functions
+        for j in $i/fish/functions/*
+        do
+            ln -s $(readlink -f $j) ~/.config/fish/functions/$(basename $j)
+            echo ln -s $(readlink -f $j) ~/.config/fish/functions/$(basename $j)
+        done
     else
         ln -s $CURDIR/$i ~/.$i
         echo ln -s $CURDIR/$i ~/.$i
