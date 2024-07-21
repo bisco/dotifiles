6855e3f1736a76e74ab6eb0c05ad70b9ed88c56d
bisco <bisco@nbis.co>
2024-07-21 05:53:17 +0000

    stop installing fish configuration

--- install.sh
+++ install.sh
@@ -6,18 +6,8 @@ DOTFILES=$(ls | grep -v install.sh)
 for i in ${DOTFILES[@]};
 do
     if [ -d $i ]; then
-        # fish-config 
-        mkdir -p ~/.config/fish
-        echo mkdir -p ~/.config/fish
-        ln -s $CURDIR/$i/fish/config.fish ~/.config/fish/config.fish
-        echo ln -s $CURDIR/$i/fish/config.fish ~/.config/fish/config.fish
-        mkdir -p ~/.config/fish/functions
-        echo mkdir -p ~/.config/fish/functions
-        for j in $i/fish/functions/*
-        do
-            ln -s $(readlink -f $j) ~/.config/fish/functions/$(basename $j)
-            echo ln -s $(readlink -f $j) ~/.config/fish/functions/$(basename $j)
-        done
+        # fish-config
+        # delete fish configuration
     else
         ln -s $CURDIR/$i ~/.$i
         echo ln -s $CURDIR/$i ~/.$i
