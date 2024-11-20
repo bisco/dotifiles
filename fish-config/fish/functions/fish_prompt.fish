7a7d4aaa6704ceab13e5281806cc8c2762461d09
bisco <bisco@nbis.co>
2024-11-20 17:50:20 +0900

    delete .emacs and fish

--- fish-config/fish/functions/fish_prompt.fish
+++
@@ -1,9 +0,0 @@
-function fish_prompt
-    set_color cyan
-    printf "%s@%s " (whoami) (hostname)
-    set_color yellow
-    printf "%s" (pwd)
-    set_color normal
-    printf "%s " (__fish_git_prompt)
-    printf "\n> "
-end
--- install.sh
+++ install.sh
@@ -5,11 +5,6 @@ DOTFILES=$(ls | grep -v install.sh)
 
 for i in ${DOTFILES[@]};
 do
-    if [ -d $i ]; then
-        # fish-config
-        # delete fish configuration
-    else
-        ln -s $CURDIR/$i ~/.$i
-        echo ln -s $CURDIR/$i ~/.$i
-    fi
+    ln -s $CURDIR/$i ~/.$i
+    echo ln -s $CURDIR/$i ~/.$i
 done
